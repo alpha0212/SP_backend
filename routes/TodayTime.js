dfe4aeb005f0cd7aafbca68e323d9006d27587d8
alpha0212 <qkrcjfgml321@naver.com>
2022-09-11 16:12:18 +0900

    Fix Bug: Permit continuous submission

--- routes/TodayTime.js
+++ routes/TodayTime.js
@@ -28,7 +28,6 @@ router.get("/byuserId/:id", async (req, res) => {
 router.post("/", validateToken, async (req, res) => {
   const post = req.body;
   post.user_id = req.user.user_id;
-  post.id = req.user.id;
   await TodayTime.create(post);
   res.json(post);
 });
