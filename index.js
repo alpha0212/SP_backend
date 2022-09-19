760087f1755d33f3751dfde4721591af2aca5ac7
alpha0212 <qkrcjfgml321@naver.com>
2022-09-20 00:24:06 +0900

    build

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const path = require("path");
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const db = require("./models");
