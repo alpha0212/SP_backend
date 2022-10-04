32b9019507d18dff281f2e117922fe852b8c0e8f
alpha0212 <qkrcjfgml321@naver.com>
2022-10-04 23:48:33 +0900

    Feat: Monday backend

---
+++ models/Mon.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataType) => {
+  const Mon = sequelize.define("Mon", {
+    WorkOne: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkTwo: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkThree: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkFour: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkFive: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkSix: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkSeven: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkEight: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkNine: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    WorkTen: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+  });
+  return Mon;
+};
